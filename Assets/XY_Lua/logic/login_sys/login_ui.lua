fa9427826c8e0ae146dc37891216160b11bc9bec
zenghongye1 <498550974@qq.com>
2019-11-08 18:15:37 +0800

    测试状态

--- Assets/XY_Lua/logic/login_sys/login_ui.lua
+++ Assets/XY_Lua/logic/login_sys/login_ui.lua
@@ -36,7 +36,7 @@ end
  ]]
  function this.Awake()
  	App = {}
- 	App.versionType = Version.Release
+ 	App.versionType = Version.Test
     local go = GameObject.Find("reporter");
     if go ~= nil then--and App.versionType ~= Version.Release then 
     	go:SetActive(true) 
